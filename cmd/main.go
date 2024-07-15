c89cf88f715a9c4a655a475a1d5a2a7d2dac75ce
Medet Akhmetov <axmetovmed@gmail.com>
2024-07-15 12:38:46 +0500

    Первая домашка (#1)
    * first commit
    
    * fixes
    
    * lis.Close()
    
    ---------
    
    Co-authored-by: Mario Scordia <marioscordia@MacBook-Pro-Mario.local>

---
+++ cmd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/marioscordia/chat/pkg/chat_v1"
+)
+
+const grpcPort = 50052
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Panicf("failed to listen: %v", err)
+	}
+	defer lis.Close()
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	chat_v1.RegisterChatV1Server(s, &server{})
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("server listening at %v", lis.Addr())
+
+		if err = s.Serve(lis); err != nil {
+			log.Panicf("failed to serve: %v", err)
+		}
+	}()
+
+	<-signalChan
+	log.Println("received shutdown signal")
+
+	s.GracefulStop()
+
+	log.Println("server shutdown complete")
+}
